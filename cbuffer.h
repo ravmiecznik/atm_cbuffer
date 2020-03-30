fda4d930a33ff44c803913b92d553c8d0692f1e4
rafal <ravmiecznk@gmail.com>
2020-03-30 21:58:58 +0200

    commented lines removed

--- cbuffer.h
+++ cbuffer.h
@@ -34,13 +34,11 @@ private:
 	cbuffer_constr_method::constr_method constr_method;
 	uint8_t local_buff_for_string_size;
 public:
-	//char* buffer_for_string;
 	volatile uint32_t available;
 	volatile uint8_t* buffer;
 	volatile uint32_t size;
 	CircBuffer(uint32_t siz, uint8_t local_buff_for_string_size=20):
 		head(0), tail(0), local_buff_for_string_size(local_buff_for_string_size), available(0), size(siz){
-		//buffer_for_string = (char*)malloc(local_buff_for_string_size);
 		buffer = (uint8_t*)malloc(size);
 		constr_method = cbuffer_constr_method::with_malloc;
 	}
@@ -48,14 +46,12 @@ public:
 		head = cbuffer->head;
 		size = cbuffer->size;
 		available = cbuffer->available;
-		//buffer_for_string = cbuffer->buffer_for_string;
 		buffer = cbuffer->buffer;
 		constr_method = cbuffer_constr_method::by_pointer;
 	}
 	~CircBuffer(){
 		switch (constr_method) {
 			case cbuffer_constr_method::with_malloc:
-				//free(buffer);
 				break;
 			default:
 				break;
@@ -158,72 +154,7 @@ public:
 		return false;
 	}
 
-//	char* gets(){
-//		/*
-//		 * max string len=50
-//		 */
-//		uint8_t cnt = 0;
-//		if(available){
-//			_delay_ms(10);
-//			//char c;
-//			buffer_for_string[cnt] = get();
-//			while(buffer_for_string[cnt] != '\r' and buffer_for_string[cnt] != '\n'){
-//				if(cnt >= local_buff_for_string_size){
-//					break;
-//				}
-//				cnt++;
-//				buffer_for_string[cnt] = get();
-//			}
-//		}
-//		else{
-//			buffer_for_string[0] = '\x00';
-//		}
-//		buffer_for_string[local_buff_for_string_size-1] = '\x00';
-//		return buffer_for_string;
-//	}
 
-//	char* gets(char * ext_buff, char str_end = '\x00'){
-//		/*
-//		 * max string len=50
-//		 */
-//		uint8_t cnt = 0;
-//		if(available){
-//			_delay_ms(10);
-//			//char c;
-//			ext_buff[cnt] = get();
-//			//while(ext_buff[cnt] and ext_buff[cnt] != additional_str_end){
-//			while(ext_buff[cnt] != str_end){
-//				if(cnt >= 50){
-//					break;
-//				}
-//				cnt++;
-//				ext_buff[cnt] = get();
-//			}
-//		}
-//		else{
-//			ext_buff[0] = '\x00';
-//		}
-//		return ext_buff;
-//	}
-
-//	char* gets(char str_end){
-//		gets(buffer_for_string, str_end);
-//		//for(int i=local_buff_for_string_size-3; i<local_buff_for_string_size; i++)
-//		//	buffer_for_string[i] = '.';
-//		buffer_for_string[local_buff_for_string_size-1] = '\0';
-//		return buffer_for_string;
-//	}
-
-//	char* copy_to_local(){
-//	/*
-//	 * Gets cbuffer content to buffer_for_string
-//	 */
-//		for(uint8_t i=0; i<local_buff_for_string_size; i++){
-//			buffer_for_string[i] = get();
-//		}
-//		buffer_for_string[local_buff_for_string_size-1] = '\x00';
-//		return buffer_for_string;
-//	}
 
 	void put(uint32_t amount, uint8_t* ext_buffer){
 		uint32_t i=0;
@@ -273,42 +204,12 @@ public:
 		return false;
 	}
 
-	//bool is_in_local(const char* string);
-
-	//bool is_in_local_p(const char* string);
-
-
-//	int32_t is_in_buffer(char c){
-//		uint8_t* tmp_ptr=buffer+head;
-//		//uint32_t position = 0;
-//		char _c;
-//		for(uint32_t i=0; i<available; i++){
-//			_c = *(tmp_ptr+i); //<!!!!! overflow of pointer
-//			if(_c == c){
-//				return i;
-//			}
-//		}
-//		return -1;
-//	}
 
 	void set_head(uint32_t new_head){
 		head = new_head;
 	}
 
-//	bool is_in_buffer(char* string){
-//		uint32_t _head = head;
-//		if(available){
-//			uint32_t i=0;
-//			while(string[i]){
-//				if(buffer[_head] != string[i])
-//					return false;
-//				i++;
-//				_head = (_head + 1) % size;
-//			}
-//			return true;
-//		}
-//		return false;
-//	}
+
 
 	volatile uint8_t* get_buff_ptr(){
 		return buffer;
