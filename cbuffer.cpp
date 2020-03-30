04ca5f437a710888b565cf3c1f24670533a124f1
rafal <ravmiecznk@gmail.com>
2020-03-30 21:58:58 +0200

    commented lines removed
    constructors definitions moved to cpp file
    
    all methods definitions moved to cpp file, get rid off uint8_t local_buff_for_string_size;
    
    unused code removed

--- cbuffer.cpp
+++ cbuffer.cpp
@@ -7,61 +7,179 @@
 
 #include "cbuffer.h"
 
-//void print_buff_char(char c){
-//	/*
-//	 * Prints char if alpha numeric
-//	 * or Hex if not
-//	 */
-//	if(isalnum(c))
-//		printf("%c", c);
-//	else if(c == '>' or c == '<')
-//		printf("%c", c);
-//	else
-//		printf("%02X ", c);
-//}
-
-//char read_pgm_byte_smart(const char* ptr){
-//	uint32_t addr = (uint32_t)ptr;
-//	if(addr < 0xFFFF)
-//		return pgm_read_byte(addr);
-//	else
-//		return pgm_read_byte_far(addr);
-//}
-//
-//bool CircBuffer::is_in_local(const char* string){
-//	/*
-//	 * check if string in local buffer
-//	 */
-//	char c;
-//	for(uint8_t i=0; i<local_buff_for_string_size; i++){
-//		c = *string++;
-//		if(c == '\x00'){
-//			break;
-//		}
-//		if(buffer_for_string[i] != c)
-//			return false;
-//	}
-//	return true;
-//}
-//
-//
-//bool CircBuffer::is_in_local_p(const char* string){
-//
-//		/*
-//		 * check if string in local buffer
-//		 */
-//		char b;
-//		for(uint8_t i=0; i<local_buff_for_string_size; i++){
-//			b=read_pgm_byte_smart(&(string[i]));
-//			if(b == '\x00'){
-//				break;
-//			}
-//			if(buffer_for_string[i] != b)
-//				return false;
-//		}
-//		return true;
-//}
-//
 
 
+CircBuffer::CircBuffer(uint32_t siz):
+	head(0), tail(0), available(0), size(siz){
+	buffer = (uint8_t*)malloc(size);
+	constr_method = cbuffer_constr_method::with_malloc;
+}
 
+CircBuffer::CircBuffer(CircBuffer* cbuffer){
+	head = cbuffer->head;
+	size = cbuffer->size;
+	available = cbuffer->available;
+	buffer = cbuffer->buffer;
+	constr_method = cbuffer_constr_method::by_pointer;
+}
+
+CircBuffer::~CircBuffer(){
+	switch (constr_method) {
+		case cbuffer_constr_method::with_malloc:
+			break;
+		default:
+			break;
+	}
+}
+
+
+uint32_t CircBuffer::free_space(){
+	return size - available;
+}
+
+void CircBuffer::flush(){
+	head = 0;
+	tail = 0;
+	available = 0;
+}
+
+void CircBuffer::flush(uint32_t amount){
+	while(amount--)
+		get();
+}
+
+CircBuffer CircBuffer::peek(){
+	/*
+	 * Return CircBuffer created with pointer constructor
+	 * It makes its own head, tail, available but points to common buffer
+	 * In practice this is just freeze state of real CircBuffer can be used to peek
+	 */
+	return CircBuffer(this);
+}
+
+void CircBuffer::peek_sync(CircBuffer* peek_cbuffer){
+	peek_cbuffer->available = available;
+	peek_cbuffer->head = head;
+	peek_cbuffer->tail = tail;
+}
+
+void CircBuffer::put(uint8_t chr){
+	buffer[tail] = chr;
+	tail = (tail + 1) % size;
+	if(available < size)
+		available++;
+	if(available == size)
+		head = tail;
+}
+
+uint8_t CircBuffer::get(){
+	if(available){
+		char c;
+		c = buffer[head];
+		available--;
+		head = (head + 1) % size;
+		return c;
+	}
+	else
+		return 0;
+}
+
+void CircBuffer::get(uint32_t amount, uint8_t* ext_buffer){
+	while(amount and available){
+		*(ext_buffer++) = get();
+		amount--;
+	}
+}
+void CircBuffer::get_reversed(uint32_t amount, uint8_t* ext_buffer){
+	while(amount and available){
+		ext_buffer[(amount--)-1] = get();
+	}
+}
+void CircBuffer::get(uint32_t amount, char* ext_buffer){
+	get(amount, (uint8_t*)ext_buffer);
+}
+
+
+//TODO: size of ext_buffer must be known!!
+char* CircBuffer::get_all(char* ext_buffer){
+	uint32_t cnt=0;
+	char c;
+	while(available){
+		c = get();
+		*(ext_buffer+ cnt++)=c;
+	}
+	ext_buffer[cnt] = get();
+	flush();
+	return ext_buffer;
+}
+
+bool CircBuffer::is_string_in_buff(){
+	if(available){
+		if(buffer[tail] == '\n' or buffer[tail] == '\n'){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	return false;
+}
+
+
+
+void CircBuffer::put(uint32_t amount, uint8_t* ext_buffer){
+	uint32_t i=0;
+	while(amount){
+		put(ext_buffer[i]);
+		amount--;
+		i++;
+	}
+}
+
+template<typename data_type>
+void CircBuffer::puts(data_type* ext_buffer){
+	uint32_t i=0;
+	while(*(ext_buffer + i) and i<size){
+		put(*(ext_buffer + i++));
+	}
+	put(*(ext_buffer + i));
+}
+
+
+bool CircBuffer::is_endl_in_buffer(){
+		volatile uint8_t* tmp_ptr=buffer+head;
+		volatile uint32_t _head = head;
+		char c;
+		for(uint32_t i=0; i<available; i++){
+			c = *(tmp_ptr+_head);
+			_head = (_head + 1) % size;
+			if( c == '\r' or c == '\n')
+				return true;
+		}
+		return false;
+}
+
+bool CircBuffer::is_in_buffer(const char* string){
+	uint32_t _head = head;
+	if(available){
+		uint32_t i=0;
+		char c;
+		while(c = pgm_read_byte(string+i)){
+			if(buffer[_head] != c)
+				return false;
+			i++;
+			_head = (_head + 1) % size;
+		}
+		return true;
+	}
+	return false;
+}
+
+
+void CircBuffer::CircBuffer::set_head(uint32_t new_head){
+	head = new_head;
+}
+
+volatile uint8_t* CircBuffer::get_buff_ptr(){
+	return buffer;
+}
--- cbuffer.h
+++ cbuffer.h
@@ -23,296 +23,38 @@ namespace cbuffer_constr_method{
 	};
 }
 
-void print_buff_char(char c);
-
-//TODO: move all methods code to *.cpp file, possibly it will save some ram
 
 class CircBuffer{
 private:
 	volatile uint32_t head;
 	volatile uint32_t tail;
 	cbuffer_constr_method::constr_method constr_method;
-	uint8_t local_buff_for_string_size;
 public:
-	//char* buffer_for_string;
 	volatile uint32_t available;
 	volatile uint8_t* buffer;
 	volatile uint32_t size;
-	CircBuffer(uint32_t siz, uint8_t local_buff_for_string_size=20):
-		head(0), tail(0), local_buff_for_string_size(local_buff_for_string_size), available(0), size(siz){
-		//buffer_for_string = (char*)malloc(local_buff_for_string_size);
-		buffer = (uint8_t*)malloc(size);
-		constr_method = cbuffer_constr_method::with_malloc;
-	}
-	CircBuffer(CircBuffer* cbuffer, uint8_t local_buff_for_string_size=20): local_buff_for_string_size(local_buff_for_string_size){
-		head = cbuffer->head;
-		size = cbuffer->size;
-		available = cbuffer->available;
-		//buffer_for_string = cbuffer->buffer_for_string;
-		buffer = cbuffer->buffer;
-		constr_method = cbuffer_constr_method::by_pointer;
-	}
-	~CircBuffer(){
-		switch (constr_method) {
-			case cbuffer_constr_method::with_malloc:
-				//free(buffer);
-				break;
-			default:
-				break;
-		}
-
-	}
-
-	uint32_t free_space(){
-		return size - available;
-	}
-
-	void flush(){
-		head = 0;
-		tail = 0;
-		available = 0;
-	}
-
-	void flush(uint32_t amount){
-		while(amount--)
-			get();
-	}
-
-	CircBuffer peek(){
-		/*
-		 * Return CircBuffer created with pointer constructor
-		 * It makes its own head, tail, available but points to common buffer
-		 * In practice this is just freeze state of real CircBuffer can be used to peek
-		 */
-		return CircBuffer(this);
-	}
-
-	void peek_sync(CircBuffer* peek_cbuffer){
-		peek_cbuffer->available = available;
-		peek_cbuffer->head = head;
-		peek_cbuffer->tail = tail;
-	}
-
-	void put(uint8_t chr){
-		buffer[tail] = chr;
-		tail = (tail + 1) % size;
-		if(available < size)
-			available++;
-		if(available == size)
-			head = tail;
-	}
-
-	uint8_t get(){
-		if(available){
-			char c;
-			c = buffer[head];
-			available--;
-			head = (head + 1) % size;
-			return c;
-		}
-		else
-			return 0;
-	}
-
-	void get(uint32_t amount, uint8_t* ext_buffer){
-		//uint32_t i=0;
-		while(amount and available){
-			*(ext_buffer++) = get();
-			amount--;
-			//i++;
-		}
-	}
-	void get_reversed(uint32_t amount, uint8_t* ext_buffer){
-		while(amount and available){
-			ext_buffer[(amount--)-1] = get();
-		}
-	}
-	void get(uint32_t amount, char* ext_buffer){
-		get(amount, (uint8_t*)ext_buffer);
-	}
-
-
-	//TODO: size of ext_buffer must be known!!
-	char* get_all(char* ext_buffer){
-		uint32_t cnt=0;
-		char c;
-		//while(c = get()){
-		while(available){
-			c = get();
-			*(ext_buffer+ cnt++)=c;
-		}
-		ext_buffer[cnt] = get();
-		flush();
-		return ext_buffer;
-	}
-
-	bool is_string_in_buff(){
-		if(available){
-			if(buffer[tail] == '\n' or buffer[tail] == '\n'){
-				return true;
-			}
-			else{
-				return false;
-			}
-		}
-		return false;
-	}
-
-//	char* gets(){
-//		/*
-//		 * max string len=50
-//		 */
-//		uint8_t cnt = 0;
-//		if(available){
-//			_delay_ms(10);
-//			//char c;
-//			buffer_for_string[cnt] = get();
-//			while(buffer_for_string[cnt] != '\r' and buffer_for_string[cnt] != '\n'){
-//				if(cnt >= local_buff_for_string_size){
-//					break;
-//				}
-//				cnt++;
-//				buffer_for_string[cnt] = get();
-//			}
-//		}
-//		else{
-//			buffer_for_string[0] = '\x00';
-//		}
-//		buffer_for_string[local_buff_for_string_size-1] = '\x00';
-//		return buffer_for_string;
-//	}
-
-//	char* gets(char * ext_buff, char str_end = '\x00'){
-//		/*
-//		 * max string len=50
-//		 */
-//		uint8_t cnt = 0;
-//		if(available){
-//			_delay_ms(10);
-//			//char c;
-//			ext_buff[cnt] = get();
-//			//while(ext_buff[cnt] and ext_buff[cnt] != additional_str_end){
-//			while(ext_buff[cnt] != str_end){
-//				if(cnt >= 50){
-//					break;
-//				}
-//				cnt++;
-//				ext_buff[cnt] = get();
-//			}
-//		}
-//		else{
-//			ext_buff[0] = '\x00';
-//		}
-//		return ext_buff;
-//	}
-
-//	char* gets(char str_end){
-//		gets(buffer_for_string, str_end);
-//		//for(int i=local_buff_for_string_size-3; i<local_buff_for_string_size; i++)
-//		//	buffer_for_string[i] = '.';
-//		buffer_for_string[local_buff_for_string_size-1] = '\0';
-//		return buffer_for_string;
-//	}
-
-//	char* copy_to_local(){
-//	/*
-//	 * Gets cbuffer content to buffer_for_string
-//	 */
-//		for(uint8_t i=0; i<local_buff_for_string_size; i++){
-//			buffer_for_string[i] = get();
-//		}
-//		buffer_for_string[local_buff_for_string_size-1] = '\x00';
-//		return buffer_for_string;
-//	}
-
-	void put(uint32_t amount, uint8_t* ext_buffer){
-		uint32_t i=0;
-		while(amount){
-			put(ext_buffer[i]);
-			amount--;
-			i++;
-		}
-	}
-
+	CircBuffer(uint32_t siz);
+	CircBuffer(CircBuffer* cbuffer);
+	~CircBuffer();
+	uint32_t free_space();
+	void flush();
+	void flush(uint32_t amount);
+	CircBuffer peek();
+	void peek_sync(CircBuffer* peek_cbuffer);
+	void put(uint8_t chr);
+	uint8_t get();
+	void get(uint32_t amount, uint8_t* ext_buffer);
+	void get_reversed(uint32_t amount, uint8_t* ext_buffer);
+	void get(uint32_t amount, char* ext_buffer);
+	char* get_all(char* ext_buffer);
+	bool is_string_in_buff();
+	void put(uint32_t amount, uint8_t* ext_buffer);
 	template<typename data_type>
-	void puts(data_type* ext_buffer){
-		uint32_t i=0;
-		while(*(ext_buffer + i) and i<size){
-			put(*(ext_buffer + i++));
-		}
-		put(*(ext_buffer + i));
-	}
-
-
-	bool is_endl_in_buffer(){
-			volatile uint8_t* tmp_ptr=buffer+head;
-			volatile uint32_t _head = head;
-			char c;
-			for(uint32_t i=0; i<available; i++){
-				c = *(tmp_ptr+_head);
-				_head = (_head + 1) % size;
-				if( c == '\r' or c == '\n')
-					return true;
-			}
-			return false;
-	}
-
-	bool is_in_buffer(const char* string){
-		uint32_t _head = head;
-		if(available){
-			uint32_t i=0;
-			char c;
-			while(c = pgm_read_byte(string+i)){
-				if(buffer[_head] != c)
-					return false;
-				i++;
-				_head = (_head + 1) % size;
-			}
-			return true;
-		}
-		return false;
-	}
-
-	//bool is_in_local(const char* string);
-
-	//bool is_in_local_p(const char* string);
-
-
-//	int32_t is_in_buffer(char c){
-//		uint8_t* tmp_ptr=buffer+head;
-//		//uint32_t position = 0;
-//		char _c;
-//		for(uint32_t i=0; i<available; i++){
-//			_c = *(tmp_ptr+i); //<!!!!! overflow of pointer
-//			if(_c == c){
-//				return i;
-//			}
-//		}
-//		return -1;
-//	}
-
-	void set_head(uint32_t new_head){
-		head = new_head;
-	}
-
-//	bool is_in_buffer(char* string){
-//		uint32_t _head = head;
-//		if(available){
-//			uint32_t i=0;
-//			while(string[i]){
-//				if(buffer[_head] != string[i])
-//					return false;
-//				i++;
-//				_head = (_head + 1) % size;
-//			}
-//			return true;
-//		}
-//		return false;
-//	}
-
-	volatile uint8_t* get_buff_ptr(){
-		return buffer;
-	}
+	void puts(data_type* ext_buffer);
+	bool is_endl_in_buffer();
+	bool is_in_buffer(const char* string);
+	void set_head(uint32_t new_head);
+	volatile uint8_t* get_buff_ptr();
 };
 
 
