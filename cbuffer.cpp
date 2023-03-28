dd42fa155c6db92b907a10364eca099e18b34aec
rafal <ravmiecznk@gmail.com>
2023-03-28 23:43:03 +0200

    simplify cbuffer implementation, overlfow fixed

--- cbuffer.cpp
+++ cbuffer.cpp
@@ -10,15 +10,18 @@
 
 
 CircBuffer::CircBuffer(uint32_t siz):
-	head(0), tail(0), available(0), size(siz){
+	head(0), tail(0), size(siz){
 	buffer = (uint8_t*)malloc(size);
 	constr_method = cbuffer_constr_method::with_malloc;
+}
+
+uint32_t CircBuffer::available(){
+	return tail>head ? tail - head : head- tail;
 }
 
 CircBuffer::CircBuffer(CircBuffer* cbuffer){
 	head = cbuffer->head;
 	size = cbuffer->size;
-	available = cbuffer->available;
 	buffer = cbuffer->buffer;
 	constr_method = cbuffer_constr_method::by_pointer;
 }
@@ -34,13 +37,12 @@ CircBuffer::~CircBuffer(){
 
 
 uint32_t CircBuffer::free_space(){
-	return size - available;
+	return size - available();
 }
 
 void CircBuffer::flush(){
 	head = 0;
 	tail = 0;
-	available = 0;
 }
 
 void CircBuffer::flush(uint32_t amount){
@@ -58,41 +60,26 @@ CircBuffer CircBuffer::peek(){
 }
 
 void CircBuffer::peek_sync(CircBuffer* peek_cbuffer){
-	peek_cbuffer->available = available;
 	peek_cbuffer->head = head;
 	peek_cbuffer->tail = tail;
 }
 
 bool CircBuffer::put(uint8_t chr){
-	buffer[tail] = chr;
-	tail = (tail + 1) % size;
-	if(available < size)
-		available++;
-	if(available == size)
-		return false;
-//		head = tail;
-	return true;
-}
 
-bool CircBuffer::put(uint32_t amount, uint8_t* ext_buffer){
-	uint32_t i=0;
-	bool buffer_status;
-	while(amount){
-		buffer_status = put(ext_buffer[i]);
-		if(buffer_status == false){
-			return false;
-		}
-		amount--;
-		i++;
+	if(free_space()){
+		buffer[tail] = chr;
+		tail = (tail + 1) % size;
+		return true;
 	}
-	return true;
+	else{
+		return false;
+	}
 }
 
 uint8_t CircBuffer::get(){
-	if(available){
+	if(available()){
 		char c;
 		c = buffer[head];
-		available--;
 		head = (head + 1) % size;
 		return c;
 	}
@@ -100,33 +87,3 @@ uint8_t CircBuffer::get(){
 		return 0;
 }
 
-void CircBuffer::get(uint32_t amount, uint8_t* ext_buffer){
-	while(amount and available){
-		*(ext_buffer++) = get();
-		amount--;
-	}
-}
-
-void CircBuffer::get(uint32_t amount, char* ext_buffer){
-	while(amount and available){
-		*(ext_buffer++) = get();
-		amount--;
-	}
-}
-
-//TODO: size of ext_buffer must be known!!
-char* CircBuffer::get_all(char* ext_buffer){
-	uint32_t cnt=0;
-	char c;
-	while(available){
-		c = get();
-		*(ext_buffer+ cnt++)=c;
-	}
-	ext_buffer[cnt] = get();
-	flush();
-	return ext_buffer;
-}
-
-volatile uint8_t* CircBuffer::get_buff_ptr(){
-	return buffer;
-}
--- cbuffer.h
+++ cbuffer.h
@@ -30,26 +30,20 @@ private:
 	volatile uint32_t tail;
 	cbuffer_constr_method::constr_method constr_method;
 public:
-	volatile uint32_t available;
+	uint32_t available();
 	volatile uint8_t* buffer;
 	volatile uint32_t size;
 	CircBuffer(uint32_t siz);
 	CircBuffer(CircBuffer* cbuffer);
 	~CircBuffer();
 	bool put(uint8_t chr);
-	bool put(uint32_t amount, uint8_t* ext_buffer);
 	uint32_t free_space();
 	void flush();
 	void flush(uint32_t amount);
 	CircBuffer peek();
 	void peek_sync(CircBuffer* peek_cbuffer);
 	uint8_t get();
-	void get(uint32_t amount, uint8_t* ext_buffer);
-	void get(uint32_t amount, char* ext_buffer);
-	char* get_all(char* ext_buffer);
-	template<typename data_type>
-	void set_head(uint32_t new_head);
-	volatile uint8_t* get_buff_ptr();
+
 };
 
 
