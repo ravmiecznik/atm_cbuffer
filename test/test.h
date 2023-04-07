761a5ae46d73f47adbbea415e66ffc126e5fa30f
rafal <ravmiecznk@gmail.com>
2023-04-07 21:48:36 +0200

    base unit tests added

---
+++ test/test.h
@@ -0,0 +1,26 @@
+/*
+ * test.h
+ *
+ *  Created on: Apr 7, 2023
+ *      Author: rafal
+ */
+
+#ifndef ATM_CBUFFER_TEST_TEST_H_
+#define ATM_CBUFFER_TEST_TEST_H_
+
+#include "../cbuffer.h"
+#include <cppunit/TestCase.h>
+#include <cppunit/TestFixture.h>
+#include <cppunit/ui/text/TextTestRunner.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/TestRunner.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/XmlOutputter.h>
+
+
+
+#endif /* ATM_CBUFFER_TEST_TEST_H_ */
